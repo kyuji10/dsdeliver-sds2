ffbc10409fa18330c471e14f408cff2d0315d0fb
Yuji Fujimoto <adm_yuji@hotmail.com>
2021-01-09 16:32:19 -0300

    Add routes and Steps

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,15 +1,11 @@
 import './App.css';
-import Home from './Home';
-import Navbar from './Navbar';
+import Routes from './Routes';
 
 function App() {
   return (
-    <div>
-      <>
-     <Navbar />
-     <Home />
-     </>
-    </div>
+
+     <Routes />
+
   );
 }
 
---
+++ front-web/src/Orders/index.tsx
@@ -0,0 +1,14 @@
+import './styles.css'
+import { ReactComponent as Logo} from './logo.svg';
+import StepsHeader from './StepsHeader';
+
+function Orders() {
+    return (
+      <div className="orders-cointainer">
+          <StepsHeader />
+      </div>
+
+    )
+}
+
+export default Orders;
